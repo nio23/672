3284cb5d849618851120216733fb3c7470529446
nio23 <47280133+nio23@users.noreply.github.com>
2024-07-11 12:50:25 +0300

    Added vertical scroll capability

--- ZigbeeMonitor/app/src/main/java/com/example/zigbeemonitor/MainActivity.kt
+++ ZigbeeMonitor/app/src/main/java/com/example/zigbeemonitor/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DeviceThermostat
 import androidx.compose.material.icons.rounded.WaterDrop
@@ -85,7 +87,9 @@ fun MainScreen(){
 
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
